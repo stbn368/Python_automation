ba0d1104af2a1cbf7a769d2b33592ec80b412144
DESKTOP-DRCIL3M\stbn_ <stbn_3@hotmail.com>
2024-12-26 11:22:39 +0100

    updating how the reports are created

--- features/environment.py
+++ features/environment.py
@@ -3,45 +3,41 @@ from configuration import Config
 import chromedriver_autoinstaller
 from datetime import datetime
 import os
+import re
 
 def before_all(context):
-    # Install automatically Chromedriver
     chromedriver_autoinstaller.install()
-    
-    # Set up driver
     context.driver = webdriver.Chrome()
     context.driver.implicitly_wait(Config.IMPLICIT_WAIT)
     context.driver.maximize_window()
 
+
 def before_scenario(context, scenario):
-    """
-    Navigate to the home page for each scenario
-    """
     context.driver.get(Config.BASE_URL)
 
-def after_scenario(context, scenario):     
-    # Generate a custom report taking into account the datetime
+
+def after_scenario(context, scenario):
     timestamp_report = datetime.now().strftime("%Y-%m-%d_%H-%M")
     timestamp_screenshot = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    scenario_name = scenario.name.replace(" ", "_")
-    report_address = os.path.join(os.path.pardir, "reports", timestamp_report)
-    screenshot_address = os.path.join(os.path.pardir, "screenshots")
-    
-    # Create a report directory if it does not exist
+    scenario_name = re.sub(r'[^\w\-_]', '_', scenario.name)
+    report_address = os.path.join(os.getcwd(), "reports", timestamp_report)
+    screenshot_address = os.path.join(os.getcwd(), "screenshots")
+
     os.makedirs(report_address, exist_ok=True)
     os.makedirs(screenshot_address, exist_ok=True)
-    
-    # Create or update the custom report
+
+    # Create or update the report
     with open(os.path.join(report_address, "custom_report.txt"), "a") as reporte:
         if scenario.status == "passed":
             reporte.write(f"Scenario '{scenario.name}' PASSED\n")
         else:
             reporte.write(f"Scenario '{scenario.name}' FAILED\n")
-            
-            # Take screenshot
-            screenshot_filename = os.path.join(screenshot_address, f"{scenario_name}_{timestamp_screenshot}.png")
-            context.driver.save_screenshot(screenshot_filename)
+            try:
+                screenshot_filename = os.path.join(screenshot_address, f"{scenario_name}_{timestamp_screenshot}.png")
+                context.driver.save_screenshot(screenshot_filename)
+            except Exception as e:
+                print(f"Error al guardar captura de pantalla: {e}")
+
 
 def after_all(context):
-    # Close driver
     context.driver.quit()
