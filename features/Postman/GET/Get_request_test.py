e99e6d8a63427c656dba531d5ee15ba9705ea256
DESKTOP-DRCIL3M\stbn_ <stbn_3@hotmail.com>
2025-01-16 16:47:22 +0100

    updating postman tests

--- features/Postman/GET/Get_request_test.py
+++ features/Postman/GET/Get_request_test.py
@@ -1,14 +1,20 @@
+import unittest
 import requests
 
-url = "https://postman-echo.com/get?param1=value1&param2=value2"
+class TestGetRequest(unittest.TestCase):
+    def test_get_request(self):
+        url = "https://postman-echo.com/get?param1=value1&param2=value2"
+        payload = {}
+        headers = {
+            'Cookie': 'sails.sid=%3AF61UiSwytRpSXAKBXL0oVqjRv8UClkks.k2UowuDmh3Rk%2B3aSA3cPqiXNvZw0gLghrp%2BgzPL4a'
+        }
 
-payload = {}
-headers = {
-  'Cookie': 'sails.sid=s%3AF61UiSwytRpSXAkBXLo0vqlRv8UClkks.k2cUowuDmh3Rk%2B3aSA3cPqiXNwZvw0gLghrp%2BgzPLa4'
-}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        
+        #print(dir(response))
+        
+        # Aserción para verificar el código de estado
+        self.assertEqual(response.status_code, 200, "Status code is not 200")
 
-response = requests.request("GET", url, headers=headers, data=payload)
-
-#print(dir(response))
-
-assert response.status_code == 200, "Status code is not 200"
+if __name__ == "__main__":
+    unittest.main()
--- features/Postman/POST/Post_request_test.py
+++ features/Postman/POST/Post_request_test.py
@@ -1,18 +1,24 @@
 import requests
 import json
+import unittest
 
-url = "https://postman-echo.com/post"
+class TestGetRequest(unittest.TestCase):
+    def test_get_request(self):
+        url = "https://postman-echo.com/post"
 
-payload = json.dumps({
-  "firstName": "Esteban",
-  "secondName": "Calderón",
-  "age": 32
-})
-headers = {
-  'Content-Type': 'application/json',
-  'Cookie': 'sails.sid=s%3AKsmfxt57chNKaoWQTI2Qcjylnj2e8rmI.Z4XdaERJoHf3XwxUbcGysqeHqkSpyuEumD6jRgBNzws'
-}
+        payload = json.dumps({
+        "firstName": "Esteban",
+        "secondName": "Calderón",
+        "age": 32
+        })
+        headers = {
+        'Content-Type': 'application/json',
+        'Cookie': 'sails.sid=s%3AKsmfxt57chNKaoWQTI2Qcjylnj2e8rmI.Z4XdaERJoHf3XwxUbcGysqeHqkSpyuEumD6jRgBNzws'
+        }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+        response = requests.request("POST", url, headers=headers, data=payload)
+        
+        assert response.status_code == 200, "Status code is not 200"
 
-assert response.status_code == 200, "Status code is not 200"
+if __name__ == "__main__":
+    unittest.main()
